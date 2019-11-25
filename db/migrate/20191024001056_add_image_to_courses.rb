afcd6304d56734a0769b9a610a2d03082b7c7416
Jordan Roszkowiak <jordanroszkowiak@gmail.com>
2019-11-25 17:59:54 +0000

    flixter commit

--- db/migrate/20191024001056_add_image_to_courses.rb
+++ db/migrate/20191024001056_add_image_to_courses.rb
@@ -1,3 +1,5 @@
-class Courses < ActiveRecord::Base
-  mount_uploader :Image, ImageUploader
+class AddImageToCourses < ActiveRecord::Migration[5.2]
+ def change
+    add_column :users, :image, :string
+  end
 end
